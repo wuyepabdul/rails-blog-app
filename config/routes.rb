fd360df66e222fe81b0443854006e59e5907662a
wuyepabdul <wuyepabdul@gmail.com>
2022-01-14 00:02:47 +0100

    Remove namspace in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,7 @@
 Rails.application.routes.draw do
-  namespace :users do
-    get '/' => 'user#index'
-    get '/:user_id' => 'users#show'
-    get '/:user_id/posts' => 'posts#index'
-    get '/:user_id/posts/:id' => 'posts#show'
-  end
-
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "mains#index"
+  get 'users/', to: 'users#index'
+  get 'users/:user_id', to: 'users#show'
+  get 'users/:user_id/posts', to: 'posts#index'
+  get 'users/:user_id/posts/:id', to: 'posts#show'
 end
