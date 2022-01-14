24b90467b8e30de5d89f1e48935fa802e90cde5e
wuyepabdul <wuyepabdul@gmail.com>
2022-01-14 23:11:24 +0100

    Remove unused files

--- db/migrate/20220114215354_add_post_index_to_likes.rb
+++
@@ -1,6 +0,0 @@
-class AddPostIndexToLikes < ActiveRecord::Migration[7.0]
-  def change
-    add_column :likes, :post_id, :string
-    add_index :likes, :post_id
-  end
-end
--- db/migrate/20220114215521_add_user_index_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class AddUserIndexToPosts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :posts, :user_id, :string
-    add_index :posts, :user_id
-  end
-end
