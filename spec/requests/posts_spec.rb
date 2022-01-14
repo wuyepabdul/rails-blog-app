be64b049e7c9c28268ee3960844bbdd745c8e490
wuyepabdul <wuyepabdul@gmail.com>
2022-01-14 03:08:41 +0100

    Add passing test for getting status code 200 on index route

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe "Posts", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "Should list all posts" do
+      # expect('you').to eq('you')
+      get user_posts_path
+      expect(response).to have_http_status(200)
+    end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
   describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+    it "Should list all users" do
+      get users
+      expect(response).to have_http_status(200)
+    end
   end
 end
