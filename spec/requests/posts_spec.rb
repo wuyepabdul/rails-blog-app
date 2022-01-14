6caa5e5b28f51f2de27acc013af490c5fd169f54
wuyepabdul <wuyepabdul@gmail.com>
2022-01-14 03:37:10 +0100

    Fix rubocop

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Posts", type: :request do
-  describe "Posts Controllers" do
-    it "Should list all posts" do
-      get user_posts_path(user_id:21)
+RSpec.describe 'Posts', type: :request do
+  describe 'Posts Controllers' do
+    it 'Should list all posts' do
+      get user_posts_path(user_id: 21)
 
       expect(response).to have_http_status(200)
       expect(response).to render_template(:index)
       expect(response.body).to include('Here is a list of created posts')
     end
 
-    it "Should display a user" do
-      get user_post_path(user_id:21, id:1)
+    it 'Should display a user' do
+      get user_post_path(user_id: 21, id: 1)
 
       expect(response).to have_http_status(200)
       expect(response).to render_template(:show)
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe "Users Controllers", type: :request do
-  describe "GET /index" do
-    it "Should list all users" do
+RSpec.describe 'Users Controllers', type: :request do
+  describe 'GET /index' do
+    it 'Should list all users' do
       get users_path
 
       expect(response).to have_http_status(200)
@@ -10,13 +10,12 @@ RSpec.describe "Users Controllers", type: :request do
       expect(response.body).to include('Here is a list of users that were created')
     end
 
-    it "Should display a user" do
-      get user_path(id:1)
+    it 'Should display a user' do
+      get user_path(id: 1)
 
       expect(response).to have_http_status(200)
       expect(response).to render_template(:show)
       expect(response.body).to include('Here is the details of a user that was created')
-
     end
   end
 end
