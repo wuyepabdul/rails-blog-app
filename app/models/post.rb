523e9d8202a53b88f4fe489d8e5ee6e49c5478ba
wuyepabdul <wuyepabdul@gmail.com>
2022-01-18 21:07:52 +0100

    Add increment_likes_counter method for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,12 @@
 class Post < ApplicationRecord
-    belongs_to: user
-    has_many: likes
-    has_many:comments
+    belongs_to :user
+    has_many :likes, :comments
 
     def most_recent_comments
         comments.last(5)
+    end
+
+    def increment_likes_counter
+        posts.likes_counter += 1
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many: posts
-    has_many: likes
-    has_many: comments
+    has_many :posts
+    has_many :likes
+    has_many :comments
 
     def most_recent_posts
         posts.last(3)
