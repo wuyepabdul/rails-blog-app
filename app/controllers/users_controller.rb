c49c0c7effec6018ad09a233393dde61227cdebe
wuyepabdul <wuyepabdul@gmail.com>
2022-01-19 21:06:29 +0100

    Add an array of dummy data in user Controller to loop through

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
-  def index; end
+  def index 
+    @posts = [
+      {'username' => 'peter druid', 'number_of_posts' => '3'},
+      {'username' => 'david armstrong', 'number_of_posts' => '1'},
+      {'username' => 'james drew', 'number_of_posts' => '2'},
+      {'username' => 'roman reigns', 'number_of_posts' => '4'},
+      {'username' => 'brock lesnar', 'number_of_posts' => '5'}
+    ]
+  end
 
   def show; end
 end
