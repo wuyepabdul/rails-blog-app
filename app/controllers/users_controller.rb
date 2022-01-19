815475dc42e7d0e2826a4236cb194b4f4b4a13d9
wuyepabdul <wuyepabdul@gmail.com>
2022-01-19 23:36:24 +0100

    Add dynamic routing for sh shows view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,25 @@
 class UsersController < ApplicationController
   def index 
     @posts = [
-      {'username' => 'peter druid', 'number_of_posts' => '3'},
-      {'username' => 'david armstrong', 'number_of_posts' => '1'},
-      {'username' => 'james drew', 'number_of_posts' => '2'},
-      {'username' => 'roman reigns', 'number_of_posts' => '4'},
-      {'username' => 'brock lesnar', 'number_of_posts' => '5'}
+      {'id' => '1', 'username' => 'peter druid', 'number_of_posts' => '3'},
+      {'id' => '2', 'username' => 'david armstrong', 'number_of_posts' => '1'},
+      {'id' => '3', 'username' => 'james drew', 'number_of_posts' => '2'},
+      {'id' => '4', 'username' => 'roman reigns', 'number_of_posts' => '4'},
+      {'id' => '5', 'username' => 'brock lesnar', 'number_of_posts' => '5'}
     ]
   end
 
-  def show; end
+  def show
+    @posts = [
+      {'id' => '1', 'username' => 'peter druid', 'number_of_posts' => '3'},
+      {'id' => '2', 'username' => 'david armstrong', 'number_of_posts' => '1'},
+      {'id' => '3', 'username' => 'james drew', 'number_of_posts' => '2'},
+      {'id' => '4', 'username' => 'roman reigns', 'number_of_posts' => '4'},
+      {'id' => '5', 'username' => 'brock lesnar', 'number_of_posts' => '5'}
+    ]
+    result_arr = @posts.select{|i| i['id'] == params[:id]}
+    print 'params =>'
+    @user_info = result_arr.reduce({}, :merge) 
+    puts @user_info.is_a? Hash
+  end
 end
