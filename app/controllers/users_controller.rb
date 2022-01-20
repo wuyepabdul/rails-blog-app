08a5780b871a2758a7f6208926a56ae7be7dade9
wuyepabdul <wuyepabdul@gmail.com>
2022-01-20 19:17:54 +0100

    Fix rubocop

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,49 +1,28 @@
 class UsersController < ApplicationController
-  def index 
+  def index
     @users = [
-      {'id' => '1', 'username' => 'peter druid', 'number_of_posts' => '3'},
-      {'id' => '2', 'username' => 'david armstrong', 'number_of_posts' => '1'},
-      {'id' => '3', 'username' => 'james drew', 'number_of_posts' => '2'},
-      {'id' => '4', 'username' => 'roman reigns', 'number_of_posts' => '4'},
-      {'id' => '5', 'username' => 'brock lesnar', 'number_of_posts' => '5'}
+      { 'id' => '1', 'username' => 'peter druid', 'number_of_posts' => '3' },
+      { 'id' => '2', 'username' => 'david armstrong', 'number_of_posts' => '1' }
     ]
   end
 
   def show
     @users = [
-      {'id' => '1', 'username' => 'peter druid', 'number_of_posts' => '3'},
-      {'id' => '2', 'username' => 'david armstrong', 'number_of_posts' => '1'},
-      {'id' => '3', 'username' => 'james drew', 'number_of_posts' => '2'},
-      {'id' => '4', 'username' => 'roman reigns', 'number_of_posts' => '4'},
-      {'id' => '5', 'username' => 'brock lesnar', 'number_of_posts' => '5'}
+      { 'id' => '1', 'username' => 'peter druid', 'number_of_posts' => '3' },
+      { 'id' => '2', 'username' => 'david armstrong', 'number_of_posts' => '1' }
     ]
-    matched_user = @users.select{|i| i['id'] == params[:id]}
+    matched_user = @users.select { |i| i['id'] == params[:id] }
     @user_info = matched_user.reduce({}, :merge)
 
     @posts = [
-      {'id' => '1', 
-        'title' => 'First Post', 
-        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure possimus aut, soluta tenetur reiciendis placeat eos sunt totam consequuntur labore quas atque ipsa rem repellendus...'
-      },
-      {'id' => '2', 
+      { 'id' => '1',
+        'title' => 'First Post',
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure , ...' },
+      { 'id' => '2',
         'title' => 'Second Post',
-        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure possimus aut, soluta tenetur reiciendis placeat eos sunt totam consequuntur labore quas atque ipsa rem repellendus...'
-      },
-
-      {'id' => '3',
-        'title' => 'Third Post',
-        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure possimus aut, soluta tenetur reiciendis placeat eos sunt totam consequuntur labore quas atque ipsa rem repellendus...'
-      },
-      {'id' => '4',
-        'title' => 'Fourth Post',
-        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure possimus aut, soluta tenetur reiciendis placeat eos sunt totam consequuntur labore quas atque ipsa rem repellendus...'
-      },
-      {'id' => '5',
-        'title' => 'Fifth Post',
-        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure possimus aut, soluta tenetur reiciendis placeat eos sunt totam consequuntur labore quas atque ipsa rem repellendus...'
-      }
+        'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure , ...' }
     ]
-    result_arr = @posts.select{|i| i['id'] == params[:user_id]}
+    result_arr = @posts.select { |i| i['id'] == params[:user_id] }
     @post_info = result_arr.reduce({}, :merge)
-  end  
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,48 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+Like.destroy_all
+
+20.times do |i|
+    puts "Creating User #{i+1}"
+    puts
+    puts "======================================="
+    user = User.create(
+        name: "User #{i+1}",
+        photo: "https://images.unsplash.com/photo-1592762224962-5f98a3a00621?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cG9zdHN8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
+        bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure possimus aut, soluta tenetur reiciendis placeat eos sunt totam consequuntur labore quas atque ipsa rem repellendus Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure possimus aut, soluta tenetur reiciendis placeat eos sunt totam consequuntur labore quas atque ipsa rem repellendus",
+        posts_counter: "#{rand(10)}"
+    )
+
+    puts "Creating Post #{i+1}"
+    puts
+    puts "======================================="
+
+    post = Post.create(
+        title: "Post #{i+1}",
+        text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure possimus aut, soluta tenetur reiciendis placeat eos sunt totam consequuntur labore quas atque ipsa rem repellendus Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid iure possimus aut, soluta te",
+        comments_counter: "#{rand(10)}",
+        likes_counter: "#{rand(10)}",
+        user_id: "#{rand(20)}"
+    )
+end
+
+10.times do |i|
+    puts "Creating Post #{i+1}"
+    puts
+    puts "======================================="
+    comment = Comment.create(
+        text:"Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        user_id:"#{rand(20)}",
+        post_id:"#{rand(20)}"
+    )
+
+    like = Like.create(
+        user_id: "#{rand(20)}",
+        post_id: "#{rand(20)}",
+    )
+end
