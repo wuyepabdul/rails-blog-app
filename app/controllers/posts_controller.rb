9f6fc40c2f0a113b93ee341dbc687900a788460c
wuyepabdul <wuyepabdul@gmail.com>
2022-01-21 16:07:51 +0100

    Use recent post method in post_controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,7 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
 
-    @posts_list = Post.all.order(created_at: :desc).recent_posts
-    # recent_posts = posts_list.recent_posts
-    @posts = @posts_list..select { |i| i['id'] == @user[:id] }
+    @posts = @user.recent_posts
   end
 
   def show
