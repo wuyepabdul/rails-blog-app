bfa448876bbcc7fb434fd1e563a76d1f03489dbe
wuyepabdul <wuyepabdul@gmail.com>
2022-01-21 15:51:47 +0100

    Remove user method in post controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,12 +2,12 @@ class PostsController < ApplicationController
   def index
     @user = User.find(params[:user_id])
 
-    posts_list = Post.all.order(created_at: :desc)
-    @posts = posts_list.select { |i| i['id'] == @user[:id] }
+    @posts_list = Post.all.order(created_at: :desc).recent_posts
+    # recent_posts = posts_list.recent_posts
+    @posts = @posts_list..select { |i| i['id'] == @user[:id] }
   end
 
   def show
-    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
   end
 end
