bc39a09c8e623eb6b99e70920c2d9c0677b53ed7
wuyepabdul <wuyepabdul@gmail.com>
2022-01-22 21:26:41 +0100

    Revert

--- app/helpers/application_helper.rb
+++
@@ -1,5 +0,0 @@
-module ApplicationHelper
-  def get_photo(user)
-    user.photo.present? ? user.photo : 'https://ui-avatars.com/api/?name=ruby&background=random'
-  end
-end
--- app/models/like.rb
+++
@@ -1,8 +0,0 @@
-class Like < ApplicationRecord
-  belongs_to :user
-  belongs_to :post, counter_cache: :likes_counter
-
-  def update_counter(value)
-    post.update('likes_counter', value)
-  end
-end
