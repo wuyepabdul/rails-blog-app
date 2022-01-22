3efddd5a107eb54f6a87ce20add390b6a05f84f4
wuyepabdul <wuyepabdul@gmail.com>
2022-01-22 21:22:34 +0100

    Revert to previous state

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true
-  validates :posts_counter, numericality: { greater_than_or_equal_to: 0 }
-
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   def most_recent_posts
-    posts.includes(:comments).last(3)
+    posts.order('created_at Asc').limit(3)
   end
 end
