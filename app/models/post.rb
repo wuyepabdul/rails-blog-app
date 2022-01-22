b721a273f9c61afb375afcc3ac886df82205f06b
wuyepabdul <wuyepabdul@gmail.com>
2022-01-22 18:55:51 +0100

    Revert changes

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user, counter_cache: :posts_counter
-  has_many :comments
-  has_many :likes
+  has_many :comments, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   def most_recent_comments
     comments.last(5)
